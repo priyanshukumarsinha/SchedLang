92ecb3de8c6a4744e7b359d83879f85c300cbea4
Priyanshu Kumar Sinha <79042642+priyanshukumarsinha@users.noreply.github.com>
2025-10-20 06:14:20 +0000

    tokens ready to be cooked

--- lexer/token.h
+++ lexer/token.h
@@ -1,4 +1,58 @@
 #ifndef TOKEN_H
 #define TOKEN_H
+#pragma once
+#include<string>
+
+enum class TokenType{
+    // UNKOWN TYPE
+    UNKNOWN,
+    
+    // Keywords
+    KW_TASK, KW_PRIORITY, KW_DEADLINE,
+
+    // Identifier
+    IDENT,
+
+    // Int Literal
+    INT,
+
+    // Symbols
+    EQ, SEMI, LBRACE, RBRACE,
+
+    // End of file
+    TOKEN_EOF
+
+};
+
+class Token{
+    TokenType type;
+    std::string lexeme;
+    int intVal; // valid if type == INT    
+    int line;
+    int col;
+
+    Token(){
+        type = TokenType::UNKNOWN;
+        lexeme = "";
+        line = 0;
+        col = 0;
+    }
+
+    Token(TokenType type, std::string lexeme, int line, int col){
+        this->type = type;
+        this->lexeme = lexeme;
+        this->intVal = 0;
+        this->line = line;
+        this->col = col;
+    }
+
+    Token(TokenType type, int intVal, int line, int col){
+        this->type = type;
+        this->intVal = intVal;
+        this->lexeme = std::to_string(intVal);
+        this->line = line;
+        this->col = col;
+    }
+};
 
 #endif
